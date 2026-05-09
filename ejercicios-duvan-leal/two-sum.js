92eceed10660be1a899608482f331ae3754350b2
Duvan leal <duvanleal65@gmail.com>
2026-05-09 13:31:30 -0500

    fix: ejemplo

---
+++ ejercicios-duvan-leal/two-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   const seen  = new Map();
+
+   for (let i = 0; i < nums.length; i++) {
+    const num = nums [i];
+    const complement = target - num;
+
+    if (seen.has(complement)) {
+        return [seen.get(complement), i]
+    }
+
+    seen.set(num, i);
+   }
+   return [];
+};
+
+twoSum([3,2,4], 6)
